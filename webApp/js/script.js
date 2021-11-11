1ec107c9dcc8298ad9295c25e997c9778b0e31f5
Lorenz <lorenz@Lorenzs-MacBook-Pro.local>
2021-11-11 22:15:23 +0100

    Changes to HTML frame: remove bad slice button, add +/- buttons to sliders, adjust placement of buttons, image scales size automatically

--- webApp/js/script.js
+++ webApp/js/script.js
@@ -1,9 +1,10 @@
 
 /* Event listener for automatically updating sliders */
-var slider = document.getElementById('bone-intensity-slider'); 
-slider.output = document.getElementById('bone-intensity-slider-output'); 
-slider.addEventListener('input', sliderChange); 
+var slider1 = document.getElementById('bone-intensity-slider'); 
+slider1.output = document.getElementById('bone-intensity-slider-output'); 
+slider1.addEventListener('input', sliderChange); 
 
+var slider1_minus =  document.getElementById('bone-intensity-slider'); 
 var slider2 = document.getElementById('liver-intensity-slider'); 
 slider2.output = document.getElementById('liver-intensity-slider-output'); 
 slider2.addEventListener('input', sliderChange); 
@@ -13,8 +14,9 @@ slider3.output = document.getElementById('blood-vessel-intensity-slider-output')
 slider3.addEventListener('input', sliderChange); 
 
 
-/* Function to update slider values */
+/* Function to update slider values when slider is clicked on */
 function sliderChange(event) {
   event.currentTarget.output.innerHTML = this.value;
   // console.log(this.value);
 }
+
