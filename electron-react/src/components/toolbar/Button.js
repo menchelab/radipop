8c0b2e650f2eb97d297bf520999940f2f1da90a4
Schumachej <92572232+Schumachej@users.noreply.github.com>
2021-12-27 09:50:19 +0100

    Added onClick handler for Toolbar buttons

--- electron-react/src/components/toolbar/Button.js
+++ electron-react/src/components/toolbar/Button.js
@@ -3,7 +3,7 @@ import '../../styles/toolbar.css';
 
 function Button(props) {
     return (
-    <button style={{backgroundColor: props.color, border: props.borderColor}} className="button-10">
+    <button onClick={props.myClick} style={{backgroundColor: props.color, border: props.borderColor}} className="button-10">
       {props.label}
     </button>
     );
