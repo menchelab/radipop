127f33a6f01fc3d665a88e5c1e80a1e7be3bcd0b
Schumachej <92572232+Schumachej@users.noreply.github.com>
2022-01-06 12:44:22 +0100

    Fixed Sidebar mask position

--- electron-react/src/components/display/Sidebar.js
+++ electron-react/src/components/display/Sidebar.js
@@ -10,16 +10,17 @@ function Sidebar(props){
     return(
       <div className="scrollbar-area col-lg-3 col-md-3">
         <span style={{textAlign: "center"}}>Slice Preview</span>
- 
+        <div className="scrollbar-area col-lg-12 col-md-12">
         {props.RadiPOPstates.slice_mask_container.map((smc,index) => {
         return <MiniCanvas
           index={index}
           key={index}
           slice_mask_container={smc}
-          RadiPOPstates={props.RadiPOPstates} 
+          RadiPOPstates={props.RadiPOPstates}
           setRadiPOPstates={p=>{props.setRadiPOPstates(p)}}
           />
         })}
+        </div>
       </div>
   );
 }
