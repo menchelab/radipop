59e043caeb768e93d00a26ec50461934a743def8
Lorenz <lorenz@Lorenzs-MacBook-Pro.local>
2022-02-15 21:02:46 +0100

    Updated flask documentation

--- electron-react/Documentation_segmenter_flask_API/html/search/all_4.js
+++ electron-react/Documentation_segmenter_flask_API/html/search/all_4.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['dcm2png_0',['dcm2png',['../namespacesegmenter__flask___a_p_i.html#a1085fa447800af4e234c6b8c91013574',1,'segmenter_flask_API']]],
-  ['debug_1',['DEBUG',['../classconfig_1_1_development_config.html#a117352cc494cc62c6b2f1882786a332c',1,'config.DevelopmentConfig.DEBUG()'],['../classconfig_1_1_testing_config.html#a117352cc494cc62c6b2f1882786a332c',1,'config.TestingConfig.DEBUG()']]],
-  ['developmentconfig_2',['DevelopmentConfig',['../classconfig_1_1_development_config.html',1,'config']]],
-  ['draw_5fon_5fimage_3',['draw_on_image',['../classradipop__gui_1_1_radi_pop_g_u_i.html#ac727958de4f2a9056b2eb1057f830a2f',1,'radipop_gui::RadiPopGUI']]],
-  ['draw_5fregion_5foutlines_4',['draw_region_outlines',['../namespacesegmentation__utils.html#a2a4c7b64bcddd9f663d917dfe7306995',1,'segmentation_utils']]],
-  ['drawonmask_5',['drawOnMask',['../namespacesegmenter__flask___a_p_i.html#a2ea3e26521cdcd63a5ec21e6a4dcb0e6',1,'segmenter_flask_API']]]
+  ['dcm2pngpreview_1',['dcm2pngPreview',['../namespacesegmenter__flask___a_p_i.html#a0b907cefd2476cd532a2ed1f06d09ffa',1,'segmenter_flask_API']]],
+  ['debug_2',['DEBUG',['../classconfig_1_1_development_config.html#a117352cc494cc62c6b2f1882786a332c',1,'config.DevelopmentConfig.DEBUG()'],['../classconfig_1_1_testing_config.html#a117352cc494cc62c6b2f1882786a332c',1,'config.TestingConfig.DEBUG()']]],
+  ['developmentconfig_3',['DevelopmentConfig',['../classconfig_1_1_development_config.html',1,'config']]],
+  ['draw_5fon_5fimage_4',['draw_on_image',['../classradipop__gui_1_1_radi_pop_g_u_i.html#ac727958de4f2a9056b2eb1057f830a2f',1,'radipop_gui::RadiPopGUI']]],
+  ['draw_5fregion_5foutlines_5',['draw_region_outlines',['../namespacesegmentation__utils.html#a2a4c7b64bcddd9f663d917dfe7306995',1,'segmentation_utils']]],
+  ['drawonmask_6',['drawOnMask',['../namespacesegmenter__flask___a_p_i.html#a2ea3e26521cdcd63a5ec21e6a4dcb0e6',1,'segmenter_flask_API']]]
 ];
--- electron-react/Documentation_segmenter_flask_API/html/search/functions_3.js
+++ electron-react/Documentation_segmenter_flask_API/html/search/functions_3.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['dcm2png_0',['dcm2png',['../namespacesegmenter__flask___a_p_i.html#a1085fa447800af4e234c6b8c91013574',1,'segmenter_flask_API']]],
-  ['draw_5fon_5fimage_1',['draw_on_image',['../classradipop__gui_1_1_radi_pop_g_u_i.html#ac727958de4f2a9056b2eb1057f830a2f',1,'radipop_gui::RadiPopGUI']]],
-  ['draw_5fregion_5foutlines_2',['draw_region_outlines',['../namespacesegmentation__utils.html#a2a4c7b64bcddd9f663d917dfe7306995',1,'segmentation_utils']]],
-  ['drawonmask_3',['drawOnMask',['../namespacesegmenter__flask___a_p_i.html#a2ea3e26521cdcd63a5ec21e6a4dcb0e6',1,'segmenter_flask_API']]]
+  ['dcm2pngpreview_1',['dcm2pngPreview',['../namespacesegmenter__flask___a_p_i.html#a0b907cefd2476cd532a2ed1f06d09ffa',1,'segmenter_flask_API']]],
+  ['draw_5fon_5fimage_2',['draw_on_image',['../classradipop__gui_1_1_radi_pop_g_u_i.html#ac727958de4f2a9056b2eb1057f830a2f',1,'radipop_gui::RadiPopGUI']]],
+  ['draw_5fregion_5foutlines_3',['draw_region_outlines',['../namespacesegmentation__utils.html#a2a4c7b64bcddd9f663d917dfe7306995',1,'segmentation_utils']]],
+  ['drawonmask_4',['drawOnMask',['../namespacesegmenter__flask___a_p_i.html#a2ea3e26521cdcd63a5ec21e6a4dcb0e6',1,'segmenter_flask_API']]]
 ];
