43d7117044a9bb37599ea9d817cb278964a1e37d
Schumachej <92572232+Schumachej@users.noreply.github.com>
2022-02-23 22:10:21 +0100

    Documentation of Log

--- electron-react/src/components/log/LogInfo.js
+++ electron-react/src/components/log/LogInfo.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "../../styles/log.css";
 
+/**
+ * @namespace log
+ */
+
+/**
+ * Creates list elements to display the current app status using
+ * the logInfo state variable.
+ *
+ * The message is defined in the LogMessage.js component.
+ *
+ * @memberof log
+ * @method Log
+ * @param {*} props RP variable from App.js
+ * @returns div
+ * @example
+ *   <Log key="Log" RP={props.RP} />
+ */
 function Log(props) {
   const status = props.RP.logInfo;
   const listItems = status
